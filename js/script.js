105087548a399f6b9c561670e46fd53650254dfb
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-20 13:35:33 +0100

    [add] Base Ticket Price

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const ageUser = parseInt(prompt('Inserisci la tua età'));
+const kmTravel = parseInt(prompt('Inserisci i chilometri da percorrere'));
+const priceKm = 0.21;
+const ticketPrice = kmTravel * priceKm;
+
+console.log(ticketPrice)
