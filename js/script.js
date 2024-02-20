15f62067f57cac5b0dea2d0adff12702bd03099a
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-02-20 14:06:09 +0100

    [add] Discounts and Price print to HTML

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 const ageUser = parseInt(prompt('Inserisci la tua età'));
 const kmTravel = parseInt(prompt('Inserisci i chilometri da percorrere'));
 const priceKm = 0.21;
-const ticketPrice = kmTravel * priceKm;
+let ticketPrice = kmTravel * priceKm;
+const adultAge = 18;
 
-console.log(ticketPrice)
+// Calcolo Sconti
+if (ageUser < adultAge) {
+  ticketPrice = ticketPrice - (ticketPrice / 100 * 20);
+} else if (ageUser >= 65) {
+  ticketPrice = ticketPrice - (ticketPrice / 100 * 40);
+}
+
+document.getElementById('output').innerHTML = 
+`
+Visto che hai ${ageUser} anni <br>
+il prezzo del tuo biglietto è di:<br>
+<strong>${ticketPrice.toFixed(2)} €</strong>
+` 
